bfe89b39ac97f8092a50d9a0e5be8e4caa52f8e8
pooja <poojamarudhu@gmail.com>
2025-09-06 22:38:34 +0530

    Day 1: Setup backend with Express and frontend with React (Vite)

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend working 🚀");
+});
+
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch(err => console.log("❌ MongoDB Error:", err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Resume Builder 🚀</h1>
+      <p>Frontend working</p>
+    </div>
+  );
+}
+
+export default App;
